f2a181551b0c2358915ab31bfa4f15e7fb81408f
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2023-07-15 19:27:31 +0500

    Bootstrap icons downloaded

---
+++ challenge-3/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+    return(
+        <div className="container">
+            <h1>Best Movies in 2023</h1>
+
+            <h2>The Social Network</h2>
+            <img src="https://www.movienewsletters.net/photos/086829R1.jpg" alt="" />
+            <p>84%</p>
+            <p></p>
+        </div>
+        );
+}
+
+export default App;
